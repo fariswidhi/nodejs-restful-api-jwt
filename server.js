540cc46ac664af30b6b259592d619feaf621841c
= <fariswidhiarta123@gmail.com>
2018-08-13 07:34:24 +0700

    x

--- server.js
+++ server.js
@@ -43,4 +43,7 @@ app.get('/', function(req, res){
 // 		}
 // 		)
 // }
-app.listen(3000, function(){ console.log('Node server listening on port 3000');});
+var port_number = server.listen(process.env.PORT || 3000);
+// app.listen(port_number);
+
+app.listen(port_number, function(){ console.log('Node server listening on port 3000');});
